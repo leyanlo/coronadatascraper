589032192c977b0f8ee0f2be6e0f2d173e1ac514
Leyan Lo <leyanlo@gmail.com>
2020-03-16 23:53:36 -0700

    Initial commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,41 @@
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
+
+import { graphql, useStaticQuery } from 'gatsby';
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export function Layout({ children }: LayoutProps): JSX.Element {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          name
+          description
+        }
+      }
+    }
+  `);
+
+  return (
+    <React.StrictMode>
+      <Helmet
+        titleTemplate={`%s - ${data.site.siteMetadata.name}`}
+        defaultTitle={data.site.siteMetadata.name}
+      >
+        <meta name="description" content={data.site.siteMetadata.description} />
+      </Helmet>
+
+      <header>{/* TODO */}</header>
+
+      <main>{children}</main>
+
+      <footer>{/* TODO */}</footer>
+    </React.StrictMode>
+  );
+}
