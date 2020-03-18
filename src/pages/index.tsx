c3fbb78bce0d8d4f562ac73106d4ba14d92ee6e2
Leyan Lo <leyanlo@gmail.com>
2020-03-18 00:39:20 -0700

    feat(api): add country selector

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import DeckGL from '@deck.gl/react';
-import React from 'react';
+import { css } from '@emotion/core';
+import React, { useEffect, useState } from 'react';
 import { StaticMap } from 'react-map-gl';
+import { useUID } from 'react-uid';
 
 import { Layout } from '../components/Layout';
 
@@ -14,7 +16,38 @@ const initialViewState = {
   bearing: 0,
 };
 
+type Country = {
+  Country: string;
+  Slug: string;
+  Provinces: string[];
+};
+
+const US: Country = {
+  Country: 'US',
+  Slug: 'us',
+  Provinces: ['California'],
+};
+
 export default function IndexPage(): JSX.Element {
+  const [countries, setCountries] = useState<{ [Slug: string]: Country }>({
+    us: US,
+  });
+  const [country, setCountry] = useState<Country>(US);
+  const countrySelectUid = useUID();
+
+  useEffect(() => {
+    fetch('https://api.covid19api.com/countries')
+      .then(res => res.json())
+      .then((data: Country[]) => {
+        const dataMap = data.reduce((acc, c) => {
+          acc[c.Slug] = c;
+          return acc;
+        }, {} as { [Slug: string]: Country });
+        setCountries(dataMap);
+        setCountry(dataMap.us);
+      });
+  }, []);
+
   return (
     <Layout>
       <DeckGL initialViewState={initialViewState} controller>
@@ -24,6 +57,43 @@ export default function IndexPage(): JSX.Element {
           height="100%"
         />
       </DeckGL>
+      <article
+        css={css`
+          position: absolute;
+          margin: 16px;
+          padding: 16px;
+          background-color: #fffc;
+        `}
+      >
+        <h2
+          css={css`
+            margin-top: 0;
+          `}
+        >
+          <a
+            href="https://covid19api.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            covid19api
+          </a>{' '}
+          demo
+        </h2>
+        <label htmlFor={countrySelectUid}>
+          Select country:{' '}
+          <select
+            id={countrySelectUid}
+            value={country.Slug}
+            css={css`
+              min-width: 256px;
+            `}
+          >
+            {Object.keys(countries).map(k => (
+              <option value={k}>{countries[k].Country}</option>
+            ))}
+          </select>
+        </label>
+      </article>
     </Layout>
   );
 }
