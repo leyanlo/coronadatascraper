52e61f00ea0d8e0cb8673e483cd1c63194139c3c
Leyan Lo <leyanlo@gmail.com>
2020-03-18 01:59:21 -0700

    feat: add scatterplot

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+import { ScatterplotLayer } from '@deck.gl/layers';
 import DeckGL from '@deck.gl/react';
 import { css } from '@emotion/core';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { StaticMap } from 'react-map-gl';
 import { useUID } from 'react-uid';
 
@@ -10,11 +11,9 @@ import { Layout } from '../components/Layout';
 const MAPBOX_ACCESS_TOKEN = process.env.GATSBY_MAPBOX_ACCESS_TOKEN;
 
 const initialViewState = {
-  longitude: -122.41669,
-  latitude: 37.7853,
-  zoom: 13,
-  pitch: 0,
-  bearing: 0,
+  longitude: -97,
+  latitude: 39,
+  zoom: 3.8,
 };
 
 type Country = {
@@ -23,17 +22,65 @@ type Country = {
   Provinces: string[];
 };
 
+type Status = {
+  Country: string;
+  Province: string;
+  Lat: number;
+  Lon: number;
+  Date: string;
+  Cases: number;
+  Status: 'confirmed' | 'deaths' | 'recovered';
+};
+
+type SelectedStatus = {
+  status: Status;
+  x: number;
+  y: number;
+};
+
 const US: Country = {
   Country: 'US',
   Slug: 'us',
   Provinces: ['California'],
 };
 
+const StatusTooltip = ({
+  selectedStatus: { status, x, y },
+}: {
+  selectedStatus: SelectedStatus;
+}): JSX.Element | null =>
+  !status ? null : (
+    <section
+      css={css`
+        position: absolute;
+        left: ${x}px;
+        top: ${y}px;
+        background-color: #fffc;
+        padding: 4px 8px;
+      `}
+    >
+      <h5
+        css={css`
+          margin: 0;
+        `}
+      >
+        {status.Province}
+      </h5>
+      <small>
+        {status.Cases} {status.Status}
+      </small>
+    </section>
+  );
+
 export default function IndexPage(): JSX.Element {
   const [countries, setCountries] = useState<{ [Slug: string]: Country }>({
     us: US,
   });
   const [country, setCountry] = useState<Country>(US);
+  const [statuses, setStatuses] = useState<Status[]>([]);
+  const [selectedStatus, setSelectedStatus] = useState<SelectedStatus | null>(
+    null,
+  );
   const countrySelectUid = useUID();
 
   useEffect(() => {
@@ -49,16 +96,60 @@ export default function IndexPage(): JSX.Element {
       });
   }, []);
 
+  useEffect(() => {
+    fetch(
+      `https://api.covid19api.com/dayone/country/${country.Slug}/status/confirmed`,
+    )
+      .then(res => res.json())
+      .then((data: Status[]) => {
+        setStatuses(data);
+      });
+  }, [country]);
+
+  const layer = useMemo(
+    () =>
+      new ScatterplotLayer({
+        id: 'scatterplot-layer',
+        data: statuses,
+        pickable: true,
+        opacity: 0.8,
+        filled: true,
+        radiusScale: 1000,
+        radiusMinPixels: 1,
+        radiusMaxPixels: 100,
+        getPosition: (d: Status) => [d.Lon, d.Lat],
+        getRadius: (d: Status) => Math.sqrt(d.Cases),
+        getFillColor: (d: Status) => [0, 124, 254],
+        onHover: ({
+          object: status,
+          x,
+          y,
+        }: {
+          object: Status;
+          x: number;
+          y: number;
+        }) => {
+          setSelectedStatus({
+            status,
+            x,
+            y,
+          });
+        },
+      }),
+    [statuses],
+  );
+
   return (
     <Layout>
-      <DeckGL initialViewState={initialViewState} controller>
+      <DeckGL initialViewState={initialViewState} controller layers={[layer]}>
         <StaticMap
           mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
           width="100%"
           height="100%"
         />
+        {selectedStatus && <StatusTooltip selectedStatus={selectedStatus} />}
       </DeckGL>
-      <article
+      <section
         css={css`
           position: absolute;
           margin: 16px;
@@ -92,17 +183,19 @@ export default function IndexPage(): JSX.Element {
           Select country:{' '}
           <select
             id={countrySelectUid}
-            value={country.Slug}
+            defaultValue={country.Slug}
             css={css`
               min-width: 256px;
             `}
           >
             {Object.keys(countries).map(k => (
-              <option value={k}>{countries[k].Country}</option>
+              <option value={k} key={k}>
+                {countries[k].Country}
+              </option>
             ))}
           </select>
         </label>
-      </article>
+      </section>
     </Layout>
   );
 }
