40bf64b1c798380e08c3242699045ca3d224b77e
Leyan Lo <leyanlo@gmail.com>
2020-05-08 01:13:15 -0700

    feat(data): remove locations that are missing coordinates

--- scripts/filterCdsData.ts
+++ scripts/filterCdsData.ts
@@ -7,19 +7,26 @@ import { CdsData, CdsDatum, FilteredCdsData, FilteredCdsDatum } from './types';
 const filterDates = (dates: CdsDatum['dates']): CdsDatum['dates'] =>
   Object.keys(dates).reduce((acc, k) => {
     const { cases, deaths } = dates[k];
-    // skip empty dates
-    if (cases || deaths || Object.keys(acc).length) {
-      acc[k] = {
-        cases,
-        deaths,
-      };
+    // skip empty dates if acc is empty
+    if (!cases && !deaths && !Object.keys(acc).length) {
+      return acc;
     }
+
+    acc[k] = {
+      cases,
+      deaths,
+    };
     return acc;
   }, {} as FilteredCdsDatum['dates']);
 
 const filterData = (data: CdsData): FilteredCdsData =>
   Object.keys(data).reduce((acc, k) => {
     const { dates, coordinates, level, population } = data[k];
+    // skip location if no coordinates
+    if (!coordinates) {
+      return acc;
+    }
+
     const filteredDates = filterDates(dates);
     acc[k] = {
       dates: filteredDates,
